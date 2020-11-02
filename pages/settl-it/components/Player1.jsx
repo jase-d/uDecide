434f2f8024499b1a537e79c4821d55b291f1146e
Jase DiNardo <jaseiscoding@gmail.com>
2020-11-02 11:33:53 -0800

    Sets up player inputs for slot style comparing

--- pages/settl-it/components/Player1.jsx
+++ pages/settl-it/components/Player1.jsx
@@ -6,19 +6,30 @@ import Player1Info from './Player1Info';
 export default class Player1 extends React.Component {
   constructor(props) {
     super(props);
+
+    this.confirm = this.confirm.bind(this);
   };
+
+  confirm() {
+    const inputs = document.getElementsByClassName(styles.player1Picks);
+    const values = Array.from(inputs).map((input) => Number(input.value));
+    console.log(inputs, values);
+  }
+
   render() {
     return (
       <div className={styles.playerContainer}>
         <Player1Info />
         <div></div>
         <div className={styles.playerInput}>
-          <div>Guess Four Digits
-            1-9
+          <div>
+            <p>Guess Four Digits</p>
+            <p>1-9 Each</p>
           </div>
           <div>
-            <input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input>
+            <input className={styles.player1Picks} type="number"></input><input className={styles.player1Picks} type="number"></input><input className={styles.player1Picks} type="number"></input><input className={styles.player1Picks} type="number"></input>
           </div>
+          <button onClick={this.confirm}>CONFIRM</button>
         </div>
       </div>
     );
--- pages/settl-it/components/Player2.jsx
+++ pages/settl-it/components/Player2.jsx
@@ -6,19 +6,28 @@ import Player2Info from './Player2Info.jsx';
 export default class Player2 extends React.Component {
   constructor(props) {
     super(props);
+    this.confirm = this.confirm.bind(this);
   };
+
+  confirm() {
+    const inputs = document.getElementsByClassName(styles.player2Picks);
+    const values = Array.from(inputs).map((input) => Number(input.value));
+    console.log(inputs, values);
+  }
   render() {
     return (
       <div className={styles.playerContainer}>
         <Player2Info />
         <div></div>
         <div className={styles.playerInput}>
-          <div>Guess Four Digits
-            1-9
+          <div>
+            <p>Guess Four Digits</p>
+            <p>1-9 Each</p>
           </div>
           <div>
-            <input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input><input className={styles.playerPicks} type="number"></input>
+            <input className={styles.player2Picks} type="number"></input><input className={styles.player2Picks} type="number"></input><input className={styles.player2Picks} type="number"></input><input className={styles.player2Picks} type="number"></input>
           </div>
+          <button onClick={this.confirm}>CONFIRM</button>
         </div>
       </div>
     );
